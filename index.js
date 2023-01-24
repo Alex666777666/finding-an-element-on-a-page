19a1c829858daf7f17c477388d7a5a2ec00f2503
Alex666777666 <aleksfnaf@gmail.com>
2023-01-24 18:22:31 +0200

    finding-an-element-on-a-page

---
+++ index.js
@@ -0,0 +1,13 @@
+const getTitleElement = title => {
+  const titleElem = document.querySelector('title')
+  console.dir(titleElem)
+}
+
+getTitleElement()
+
+const getInputElement = text => {
+  const inputElem = document.querySelector('input[type=text]')
+  console.dir(inputElem)
+}
+
+getInputElement()
