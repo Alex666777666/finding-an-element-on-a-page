090008e2ffcd6c86010007a6d74d3a2dd23ad49f
Alex666777666 <111201814+Alex666777666@users.noreply.github.com>
2023-01-24 18:25:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const getTitleElement = title => {
+export const getTitleElement = title => {
   const titleElem = document.querySelector('title')
   console.dir(titleElem)
 }
 
 getTitleElement()
 
-const getInputElement = text => {
+export const getInputElement = text => {
   const inputElem = document.querySelector('input[type=text]')
   console.dir(inputElem)
 }
